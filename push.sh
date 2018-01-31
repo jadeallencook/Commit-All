3e79218da5ff4867ef4f56380614d36915bc3dba
Jade Allen Cook <jcook@deseretnews.com>
2018-01-31 11:48:21 -0700

    ❌ Check for errors

--- push.sh
+++ push.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-git add "$1"
-git commit -m "$2"
-git push
+if [[ -z $1 || -z $2 ]]; then
+  echo "Looks like you forgot something..."
+else
+  git add "$1"
+  git commit -m "$2"
+  git push
+fi
